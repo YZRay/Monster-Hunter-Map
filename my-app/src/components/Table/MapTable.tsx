ef15b81041d8377d5e5cab8f3cc7d207dcd69b41
YZRay <yao19991119@gmail.com>
2023-11-08 18:38:56 +0800

    調整最多只顯示三隻魔物

--- my-app/src/components/Table/MapTable.tsx
+++ my-app/src/components/Table/MapTable.tsx
@@ -40,70 +40,73 @@ const MapTable: FC<MapTableProps> = ({ data, monster, city }) => {
       })
     : [];
 
-  const locationTable = filteredData.map((item) => (
-    <div
-      className="flex flex-col text-base lg:text-lg font-bold bg-slate-300 text-slate-800 rounded-md shadow-md p-4 hover:bg-slate-800 hover:text-slate-200 duration-300 cursor-[url('/assets/icons/mh_hand.svg'),_pointer]"
-      key={item.id}
-      onClick={() => copyTextToClipboard(`${item.coordinates}`)}
-    >
-      <div className="justify-around flex-wrap">
-        <div className="flex gap-4 relative items-center">
-          {item.name.split(",").map((monsterName, index) => {
-            return (
-              <div className="max-w-[4rem] max-h-[4rem]" key={index}>
-                <Image
-                  className="cursor-[url('/assets/icons/mh_hand.svg'),_pointer] w-auto h-auto drop-shadow"
-                  src={`/assets/icons/Monster/${monsterName}.svg`}
-                  width={50}
-                  height={50}
-                  alt="equipment"
-                  loading="lazy"
-                />
+  const locationTable = filteredData.map((item) => {
+    const monsterNames = item.name.split(",").slice(0, 3);
+    const imageElements = monsterNames.map((monsterName, index) => (
+      <div className="max-w-[4rem] max-h-[4rem]" key={index}>
+        <Image
+          className="cursor-[url('/assets/icons/mh_hand.svg'),_pointer] w-auto h-auto drop-shadow"
+          src={`/assets/icons/Monster/${monsterName}.svg`}
+          width={50}
+          height={50}
+          alt="equipment"
+          loading="lazy"
+        />
+      </div>
+    ));
+
+    return (
+      <div
+        className="flex flex-col text-base lg:text-lg font-bold bg-slate-300 text-slate-800 rounded-md shadow-md p-4 hover:bg-slate-800 hover:text-slate-200 duration-300 cursor-[url('/assets/icons/mh_hand.svg'),_pointer]"
+        key={item.id}
+        onClick={() => copyTextToClipboard(`${item.coordinates}`)}
+      >
+        <div className="justify-around flex-wrap">
+          <div className="flex gap-4 relative items-center">
+            {imageElements}
+            <div className="basis-1/2">
+              <div className="flex gap-1">
+                {Array.from(
+                  { length: item.level > 5 ? item.level - 5 : item.level },
+                  (_, index) => (
+                    <StarIcon
+                      key={index}
+                      className={`w-5 h-5 drop-shadow-md ${
+                        item.level > 5 ? "text-purple-600" : "text-amber-400"
+                      }`}
+                    />
+                  )
+                )}
               </div>
-            );
-          })}
-          <div className="basis-1/2">
-            <div className="flex gap-1">
-              {Array.from(
-                { length: item.level > 5 ? item.level - 5 : item.level },
-                (_, index) => (
-                  <StarIcon
-                    key={index}
-                    className={`w-5 h-5 drop-shadow-md ${
-                      item.level > 5 ? "text-purple-600" : "text-amber-400"
-                    }`}
-                  />
-                )
-              )}
+              <div>
+                <p className="text-base">{monsterNames.join(", ")}</p>
+                <p className="text-base">
+                  {(() => {
+                    const date = new Date(item.createdAt + "Z");
+                    const localTime = date.toLocaleString(undefined, {
+                      hour12: false,
+                    });
+                    return localTime;
+                  })()}
+                </p>
+                <span className="text-base">{item.round} 周目</span>
+              </div>
             </div>
-            <div>
-              <p className="text-base">{item.name}</p>
-              <p className="text-base">
-                {(() => {
-                  const date = new Date(item.createdAt + "Z");
-                  const localTime = date.toLocaleString(undefined, {
-                    hour12: false,
-                  });
-                  return localTime;
-                })()}
-              </p>
-              <span className="text-base">{item.round} 周目</span>
-            </div>
+            <ClipboardDocumentIcon
+              title="複製"
+              className="w-6 h-6 cursor-[url('/assets/icons/mh_hand.svg'),_pointer] absolute top-0 right-0"
+            />
           </div>
-          <ClipboardDocumentIcon
-            title="複製"
-            className="w-6 h-6 cursor-[url('/assets/icons/mh_hand.svg'),_pointer] absolute top-0 right-0"
-          />
-        </div>
-        <div className="flex items-center gap-1 ">
-          <MapPinIcon className="w-5 h-8" />
-          <span className="text-base">
-            {item.location} - {item.coordinates}
-          </span>
+          <div className="flex items-center gap-1 ">
+            <MapPinIcon className="w-5 h-8" />
+            <span className="text-base">
+              {item.location} - {item.coordinates}
+            </span>
+          </div>
         </div>
       </div>
-    </div>
-  ));
+    );
+  });
 
   return (
     <div className="mt-2 mb-4 md:mb-8 md:mt-4 lg:mb-16">
