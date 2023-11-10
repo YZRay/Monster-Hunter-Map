7adad2c5955c069bad5dd6bc5400225747d9e4f5
YZRay <yao19991119@gmail.com>
2023-11-10 19:59:45 +0800

    設定metadata

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -5,8 +5,43 @@ import GoogleAnalytics from "../components/api/GA";
 import Adsense from "../components/api/Adsense";
 
 export const metadata: Metadata = {
+  openGraph: {
+    title: "MHNow 魔物地圖",
+    description: "歡迎上傳你看到的魔物...方便大家找怪喔",
+    url: "https://mhnow.cc/",
+    siteName: "MHNow 魔物地圖",
+    locale: "zh_TW",
+    type: "website",
+    images: [
+      {
+        url: "/assets/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "MHNow 魔物地圖",
+      },
+    ],
+  },
   title: "MHNow 魔物地圖",
   description: "歡迎上傳你看到的魔物...方便大家找怪喔",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/assets/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      url: "/assets/favicon-16x16.png",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      url: "/assets/apple-touch-icon.png",
+    },
+  ],
 };
 
 export default function RootLayout({
