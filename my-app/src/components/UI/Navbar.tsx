45a3f3e58f216ce9d8c766ea59d976ea7f329206
Lion Lai <lyc1983@gmail.com>
2023-11-10 22:50:15 +0800

    修改 navbar class active 屬性

--- my-app/src/components/UI/Navbar.tsx
+++ my-app/src/components/UI/Navbar.tsx
@@ -3,21 +3,25 @@ import React, { Fragment } from "react";
 import Image from "next/image";
 import FieldIcon from "../../../public/assets/icons/field_icon.svg";
 import MonsterIcon from "../../../public/assets/icons/monster_icon.svg";
+import {usePathname} from 'next/navigation';
+
+
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <Fragment>
       <div className="container flex my-4 bg-slate-400 p-1 rounded-md gap-1 md:gap-4 top-0 z-30 shadow-lg bg-opacity-95">
         <Link
           href="/"
-          className="monster-tab flex items-center md:gap-2 text-lg"
+          className={`${pathname === '/' ? 'active' : ''} monster-tab flex items-center md:gap-2 text-lg`}
         >
           <Image src={FieldIcon} alt="FieldIcon" width={40} height={40} />
           地圖資訊
         </Link>
         <Link
           href="/SelectionPage"
-          className="monster-tab flex items-center gap-2 text-lg"
+          className={`${pathname === '/SelectionPage' ? 'active' : ''} monster-tab flex items-center gap-2 text-lg`}
         >
           <Image src={MonsterIcon} alt="MonsterIcon" width={40} height={40} />
           魔物資訊
