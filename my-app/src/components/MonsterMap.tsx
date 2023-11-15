770bf7b1a73f184f662237ee97143a5ec556c020
YZRay <yao19991119@gmail.com>
2023-11-15 20:18:36 +0800

    調整當前位置icon

--- my-app/src/components/MonsterMap.tsx
+++ my-app/src/components/MonsterMap.tsx
@@ -3,6 +3,7 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "leaflet-defaulticon-compatibility";
 import { LatLngTuple, Icon } from "leaflet";
 import { FC } from "react";
+import { MapPinIcon } from "@heroicons/react/24/solid";
 import { StarIcon } from "@heroicons/react/24/solid";
 
 interface Props {
@@ -17,21 +18,27 @@ const MonsterMap: FC<Props> = ({ geolocation, data, monster }) => {
   const latitude = geolocation?.latitude || 25.033671;
   const longitude = geolocation?.longitude || 121.564427;
   const position: LatLngTuple = [latitude, longitude];
+
   const processedData =
     data?.data.map((item) => ({
       ...item,
       monsterNames: item.name.split(",").slice(0, 1).join(""),
     })) || [];
 
-  const getMonsterIconPath = (monsterName: string) => {
-    return `/assets/icons/Monster/${monsterName}.svg`;
-  };
   //魔物圖示
   const monsterIcon = (name: string) =>
     new Icon({
-      iconUrl: getMonsterIconPath(name),
+      iconUrl: `/assets/icons/Monster/${name}.svg`,
       iconSize: [45, 45], // 設定圖案大小
       iconAnchor: [15, 15], // 設定錨點位置
+    });
+
+  //當前位置的icon
+  const curlocationIcon = () =>
+    new Icon({
+      iconUrl: `/assets/icons/location.svg`,
+      iconSize: [40, 40], // 設定圖案大小
+      iconAnchor: [20, 20], // 設定錨點位置
     });
 
   return (
@@ -47,9 +54,11 @@ const MonsterMap: FC<Props> = ({ geolocation, data, monster }) => {
         attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
         url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
       />
-      <Marker position={position}>
+      {/* 當前位置 */}
+      <Marker position={position} icon={curlocationIcon()}>
         <Popup>當前位置</Popup>
       </Marker>
+      {/* 魔物位置 */}
       {processedData.map((monsterData) => (
         <Marker
           key={monsterData.id}
