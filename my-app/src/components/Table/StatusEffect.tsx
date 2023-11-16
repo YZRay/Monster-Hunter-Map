78c8a90418377d500622f7063310e5f8bc31319c
YZRay <yao19991119@gmail.com>
2023-11-16 21:41:25 +0800

    更新魔物資料

--- my-app/src/components/Table/StatusEffect.tsx
+++ my-app/src/components/Table/StatusEffect.tsx
@@ -1,15 +1,56 @@
-import React, { FC, useState, Fragment } from "react";
+import React, { FC, Fragment } from "react";
 import Image from "next/image";
+import { StarIcon } from "@heroicons/react/24/solid";
 
 interface MyComponentProps<T> {
   data: T;
 }
-const MonsterStatusEffect: FC<MyComponentProps<any>> = () => {
+
+interface statusEffect {
+  poison: number;
+  sleep: number;
+  paralysis: number;
+  blastblight: number;
+  stun: number;
+}
+const MonsterStatusEffect: FC<MyComponentProps<any>> = ({ data }) => {
+  const statusEffects = data.statusEffect;
+
+  const renderStatusEffectRow = (effectName: string, effectCount: number) => (
+    <tr key={effectName} className="bg-gray-800">
+      <td>
+        <Image
+          src={`/assets/icons/Elements/${effectName}.svg`}
+          width={40}
+          height={40}
+          loading="lazy"
+          alt={effectName}
+          className="mx-auto"
+        />
+      </td>
+      <td>
+        <div className="flex items-center gap-1 justify-center">
+          {Array.from({ length: effectCount }, (_, index) => (
+            <StarIcon
+              key={index}
+              className="h-5 w-5 text-yellow-500"
+              aria-hidden="true"
+            />
+          ))}
+        </div>
+      </td>
+    </tr>
+  );
+
   return (
     <Fragment>
-      <p className="text-center text-md font-bold text-gray-800">
-        的數量越多，對魔物越有效
-      </p>
+      <div className="flex items-center">
+        <StarIcon className="h-5 w-5 text-yellow-500" aria-hidden="true" />
+        <p className="text-center text-md font-bold text-gray-800">
+          的數量越多，對魔物越有效
+        </p>
+      </div>
+
       <table className="monster-table">
         <thead className="bg-gray-800 text-gray-300">
           <tr>
@@ -18,71 +59,9 @@ const MonsterStatusEffect: FC<MyComponentProps<any>> = () => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-gray-800">
-            <td>
-              <Image
-                src="/assets/icons/Elements/blastblight.svg"
-                width={40}
-                height={40}
-                loading="lazy"
-                alt="fire"
-                className="mx-auto"
-              />
-            </td>
-            <td>10</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td>
-              <Image
-                src="/assets/icons/Elements/paralysis.svg"
-                width={40}
-                height={40}
-                loading="lazy"
-                alt="fire"
-                className="mx-auto"
-              />
-            </td>
-            <td>20</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td>
-              <Image
-                src="/assets/icons/Elements/stun.svg"
-                width={40}
-                height={40}
-                loading="lazy"
-                alt="fire"
-                className="mx-auto"
-              />
-            </td>
-            <td>20</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td>
-              <Image
-                src="/assets/icons/Elements/sleep.svg"
-                width={40}
-                height={40}
-                loading="lazy"
-                alt="fire"
-                className="mx-auto"
-              />
-            </td>
-            <td>20</td>
-          </tr>
-          <tr className="bg-gray-800">
-            <td>
-              <Image
-                src="/assets/icons/Elements/poison.svg"
-                width={40}
-                height={40}
-                loading="lazy"
-                alt="fire"
-                className="mx-auto"
-              />
-            </td>
-            <td>20</td>
-          </tr>
+          {Object.entries(statusEffects).map(([effectName, effectCount]) =>
+            renderStatusEffectRow(effectName, effectCount as number)
+          )}
         </tbody>
       </table>
     </Fragment>
