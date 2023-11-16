47b872caa16554ec8fa72ae72cf4389c346fd014
Lion Lai <lyc1983@gmail.com>
2023-11-16 09:16:33 +0800

    調整 map 的參數

--- my-app/src/components/MonsterMap.tsx
+++ my-app/src/components/MonsterMap.tsx
@@ -51,8 +51,8 @@ const MonsterMap: FC<Props> = ({ geolocation, data, monster }) => {
       className="w-full h-[30rem] max-h-[30rem] z-0 mb-12"
     >
       <TileLayer
-        attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/about" target="_blank">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {/* 當前位置 */}
       <Marker position={position} icon={curlocationIcon()}>
