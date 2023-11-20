135bd86da1c840bbeb2cb17f06157a620fe7bd6c
YZRay <yao19991119@gmail.com>
2023-11-20 21:12:06 +0800

    調整uuid判斷

--- my-app/src/pages/home.tsx
+++ my-app/src/pages/home.tsx
@@ -2,12 +2,10 @@
 import React, { FC, Fragment } from "react";
 import MapSelection from "@/components/MapSelection";
 import Navbar from "../components/UI/Navbar";
-
 import useUserId from "@/components/ID/UserId";
 
 const HomePage: FC = () => {
   const userId = useUserId();
-  console.log(userId);
 
   return (
     <Fragment>
