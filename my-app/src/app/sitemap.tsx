36ddd33f2704595861d9b8cc1e38fe0bc21e55f6
YZRay <yao19991119@gmail.com>
2023-11-21 23:55:47 +0800

    調整sitemap

--- my-app/src/app/sitemap.tsx
+++ my-app/src/app/sitemap.tsx
@@ -1,42 +1,42 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://mhnow.cc',
+      url: "https://mhnow.cc",
       lastModified: new Date(),
-      changeFrequency: 'always',
+      changeFrequency: "always",
       priority: 1,
     },
     {
-      url: 'https://mhnow.cc/about',
+      url: "https://mhnow.cc/about",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: 'https://mhnow.cc/news',
+      url: "https://mhnow.cc/news",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: 'https://mhnow.cc/selectionpage',
+      url: "https://mhnow.cc/equipment",
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.5,
     },
     {
-      url: 'https://mhnow.cc/privacy',
+      url: "https://mhnow.cc/privacy",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.5,
     },
     {
-      url: 'https://mhnow.cc/terms',
+      url: "https://mhnow.cc/terms",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.5,
-    }
-  ]
+    },
+  ];
 }
