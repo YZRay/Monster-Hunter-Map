fb98b0a1c9635d423c8e84ef31119ff4ccbe8460
Lion Lai <lyc1983@gmail.com>
2023-11-21 09:25:23 +0800

    修改sitemap

--- my-app/src/app/sitemap.tsx
+++ my-app/src/app/sitemap.tsx
@@ -15,10 +15,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: 'https://mhnow.cc/news',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
       url: 'https://mhnow.cc/selectionpage',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
+    {
+      url: 'https://mhnow.cc/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://mhnow.cc/terms',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    }
   ]
 }
