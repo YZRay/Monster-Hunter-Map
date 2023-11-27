355781a759861900d863934ba8ad790e870c63ae
Lion Lai <lyc1983@gmail.com>
2023-11-27 19:38:42 +0800

    SelectionPage 重定向到 equipment

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode : false
+    reactStrictMode : false,
+    async redirects() {
+      return [
+          {
+              source: '/SelectionPage',
+              destination: '/equipment',
+              permanent: true,
+          },
+      ];
+  },
 }
 
 module.exports = nextConfig
