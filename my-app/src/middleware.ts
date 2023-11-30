571038b3ae94b2d33288248231e118926756efa9
Lion Lai <lyc1983@gmail.com>
2023-11-30 16:20:26 +0800

    修改讓ads.txt可以被人抓到

--- my-app/src/middleware.ts
+++ my-app/src/middleware.ts
@@ -34,7 +34,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   // Do not run the middleware on the following paths
   matcher:
-    "/((?!api|_next/static|_next/image|manifest.json|assets|favicon.ico).*)",
+    "/((?!api|_next/static|_next/image|manifest.json|assets|favicon.ico|ads.txt).*)",
 };
 // export const config = {
 //   matcher: "/((?!api|static|.*..*|_next).*)",
