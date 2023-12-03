8144e30703bd109203113a3c29af3e4dfb0430e8
Lion Lai <lyc1983@gmail.com>
2023-12-03 16:15:39 +0800

    加上喝咖啡斗內按鈕

--- my-app/src/app/[lng]/about/page.tsx
+++ my-app/src/app/[lng]/about/page.tsx
@@ -8,6 +8,15 @@ const AboutPage: FC = () => {
     <main>
       <div className="container">
         <h1 className="text-lg">關於我們</h1>
+        <p className="my-5 flex items-center gap-2 text-lg"
+        >
+          <img
+              src="/assets/img/about.jpg"
+              alt="加入好友"
+              width="50%"
+              height="24"
+            ></img>
+        </p>
         <p className="my-5">
           我們是一群熱愛魔物獵人的玩家，因為喜愛所以熱於分享
           <br />
@@ -16,6 +25,13 @@ const AboutPage: FC = () => {
         <p className="my-5">
           如果有合作需求，歡迎寄信到 :{" "}
           <Link href="mailto:josh@mhnow.cc">josh@mhnow.cc</Link>
+        </p>
+        <p className="my-5">
+          如果覺得我們做的不錯 :{" "}
+          <Link href="https://p.ecpay.com.tw/D5FEAC2"          
+            target="_blank"
+            className={`monster-tab-mobile flex items-center gap-2 text-lg`}
+          >歡迎請我們喝杯咖啡</Link>
         </p>
         <p className="my-5">
           <Link
