f689956d478ca6665e1a9bc5510d5725c54d55cf
YZRay <yao19991119@gmail.com>
2023-12-03 19:29:37 +0800

    增加咖啡icon

--- my-app/src/app/[lng]/about/page.tsx
+++ my-app/src/app/[lng]/about/page.tsx
@@ -2,20 +2,20 @@
 import "../globals.css";
 import React, { FC, Fragment } from "react";
 import Link from "next/link";
+import { FaCoffee } from "react-icons/fa";
 
 const AboutPage: FC = () => {
   return (
     <main>
       <div className="container">
         <h1 className="text-lg">關於我們</h1>
-        <p className="my-5 flex items-center gap-2 text-lg"
-        >
+        <p className="my-5 flex items-center gap-2 text-lg">
           <img
-              src="/assets/img/about.jpg"
-              alt="加入好友"
-              width="50%"
-              height="24"
-            ></img>
+            src="/assets/img/about.jpg"
+            alt="加入好友"
+            width="50%"
+            height="24"
+          ></img>
         </p>
         <p className="my-5">
           我們是一群熱愛魔物獵人的玩家，因為喜愛所以熱於分享
@@ -28,10 +28,14 @@ const AboutPage: FC = () => {
         </p>
         <p className="my-5">
           如果覺得我們做的不錯 :{" "}
-          <Link href="https://p.ecpay.com.tw/D5FEAC2"          
+          <Link
+            href="https://p.ecpay.com.tw/D5FEAC2"
             target="_blank"
-            className={`monster-tab-mobile flex items-center gap-2 text-lg`}
-          >歡迎請我們喝杯咖啡</Link>
+            className="flex w-fit items-center btn gap-2 text-base rounded-md font-bold py-2 px-4 mt-2"
+          >
+            <FaCoffee />
+            歡迎請我們喝杯咖啡
+          </Link>
         </p>
         <p className="my-5">
           <Link
