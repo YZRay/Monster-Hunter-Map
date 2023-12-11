f0ced4f51cd0aa54eb88c4895c51afa2b6924bb0
Lion Lai <lyc1983@gmail.com>
2023-12-11 12:05:58 +0800

    拿掉沒用到的部分

--- my-app/src/app/[lng]/home/page.tsx
+++ my-app/src/app/[lng]/home/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import React, { FC, Fragment } from "react";
 import MapSelection from "@/components/MapSelection";
-import useUserId from "@/components/Hook/UserId";
 
 const HomePage: FC = () => {
-  const userId = useUserId();
-
   return (
     <Fragment>
       <div className="container">
