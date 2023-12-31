49ad27250f6cd3e9d1154b92ea26bce9ee611c66
YZRay <yao19991119@gmail.com>
2023-12-31 23:06:59 +0800

    style: 調整sidebar寬度

--- my-app/src/app/[lng]/news/[slug]/page.tsx
+++ my-app/src/app/[lng]/news/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import { allPosts } from "contentlayer/generated";
-
+import Link from "next/link";
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
 
@@ -14,15 +14,22 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
   if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
 
   return (
-    <article className="container mb-8">
-      <div className="mb-8 text-center">
-        <h1 className="text-3xl font-bold">{post.title}</h1>
-      </div>
-      <div
-        className="[&>*]:mb-3 [&>*:last-child]:mb-0"
-        dangerouslySetInnerHTML={{ __html: post.body.html }}
-      />
-    </article>
+    <div className="container grid grid-cols-12 gap-7">
+      <article className="mb-8 col-span-9">
+        <div className="mb-8 text-center">
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+        </div>
+        <div
+          className="[&>*]:mb-3 [&>*:last-child]:mb-0"
+          dangerouslySetInnerHTML={{ __html: post.body.html }}
+        />
+      </article>
+      <aside className="col-span-3">
+        <div className="hidden lg:sticky lg:top-28 lg:block">
+          <Link href="/news">返回最新消息</Link>
+        </div>
+      </aside>
+    </div>
   );
 };
 
--- my-app/src/app/[lng]/news/page.tsx
+++ my-app/src/app/[lng]/news/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 // import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { Card, CardHeader, Button, CardFooter, Image } from "@nextui-org/react";
 import { allPosts, Post } from "contentlayer/generated";
@@ -15,7 +14,7 @@ function PostCard(post: Post) {
     <article>
       <Card
         isFooterBlurred
-        className="w-full h-[400px] relative z-0 col-span-12 sm:col-span-5 group rounded-md"
+        className="w-full h-[400px] relative z-0 group rounded-md"
       >
         <CardHeader className="absolute z-[1] top-0 flex-col items-start backdrop-blur-sm bg-white/30 ">
           <p className="text-tiny text-slate-900 uppercase font-bold">New</p>
@@ -40,7 +39,7 @@ function PostCard(post: Post) {
             <p className="text-slate-900 text-tiny">Get notified.</p> */}
           </div>
           <Button
-            className="text-base px-4 py-2 rounded-full bg-slate-300 dark:bg-slate-600"
+            className="text-base px-4 py-2 rounded-full bg-slate-300 dark:bg-slate-600 hover:scale-105 duration-300"
             color="primary"
             radius="full"
             size="sm"
@@ -65,13 +64,15 @@ export default function Home() {
         <h1 className="text-4xl font-bold text-slate-800 dark:text-slate-300 text-center">
           最新消息
         </h1>
-        <div className="grid grid-cols-12 gap-4 my-4 md:my-8">
+        <div className="grid grid-cols-12 gap-7 my-4 md:my-8">
           <div className="col-span-12 xl:col-span-9 flex flex-col gap-6">
             {posts.map((post, idx) => (
               <PostCard key={idx} {...post} />
             ))}
           </div>
-          <div className="xl:col-span-3">sidebar</div>
+          <aside className="col-span-3">
+            <div className="hidden lg:sticky lg:top-28 lg:block">SIDE BAR</div>
+          </aside>
         </div>
       </div>
     </main>
