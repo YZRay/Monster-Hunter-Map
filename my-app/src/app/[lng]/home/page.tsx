1f4b02cb0a41bcc666917fffeeb344996130f073
YZRay <yao19991119@gmail.com>
2024-01-02 18:46:23 +0800

    新增開啟discord 按鈕

--- my-app/src/app/[lng]/home/page.tsx
+++ my-app/src/app/[lng]/home/page.tsx
@@ -5,14 +5,12 @@ import DiscordBot from "@/components/DiscordBot";
 
 const HomePage: FC = () => {
   return (
-    <div className="grid grid-cols-12 gap-4 w-11/12 mx-auto">
-      <div className="xl:col-span-3">
-        <DiscordBot />
-      </div>
-      <div className="col-span-12 xl:col-span-9 w-full xl:w-3/4">
+    <Fragment>
+      <DiscordBot />
+      <div className="container">
         <MapSelection />
       </div>
-    </div>
+    </Fragment>
   );
 };
 
--- my-app/src/components/DiscordBot.tsx
+++ my-app/src/components/DiscordBot.tsx
@@ -1,21 +1,49 @@
 "use client";
-import { FC, Fragment } from "react";
+import { FC, Fragment, useState } from "react";
 import WidgetBot from "@widgetbot/react-embed";
 import { useTranslation } from "react-i18next";
+import { BsDiscord } from "react-icons/bs";
+import { Tooltip, Button } from "@nextui-org/react";
 
 const DiscordBot: FC = () => {
+  const [show, setShow] = useState(false);
+  const toggleShow = () => {
+    setShow(!show);
+  };
   const { t } = useTranslation("monster");
   const { t: transMonster } = useTranslation("data");
 
   return (
-    <div className="sticky top-16">
-      <WidgetBot
-        server="1191603191075311646"
-        channel="1191603191515729993"
-        height={800}
-        width={350}
-      />
-    </div>
+    <Fragment>
+      <Tooltip
+        placement="top"
+        content="一起來聊天吧！"
+        className="z-50 bg-slate-100 rounded-lg text-slate-800"
+      >
+        <Button
+          isIconOnly
+          color="warning"
+          variant="faded"
+          aria-label="Chat Message"
+          onClick={() => setShow(!show)}
+          className="fixed bottom-10 left-6 rounded-full p-3 bg-slate-100 drop-shadow-md z-50"
+        >
+          <BsDiscord className="w-8 h-8 text-slate-700" />
+        </Button>
+      </Tooltip>
+      <div
+        className={`fixed top-0 xl:top-20 z-40 transition-all duration-500 ${
+          show ? "right-0" : "-right-full"
+        }`}
+      >
+        <WidgetBot
+          server="1191603191075311646"
+          channel="1191603191515729993"
+          height={800}
+          className="w-[300px] xl:w-[400px] h-full"
+        />
+      </div>
+    </Fragment>
   );
 };
 
