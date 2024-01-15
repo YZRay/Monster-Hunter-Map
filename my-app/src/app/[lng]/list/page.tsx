79ddb7b11b0dfc1c32848fe46031acfc9147d8ea
YZRay <yao19991119@gmail.com>
2024-01-15 20:54:34 +0800

    style: 調整list RWD樣式

--- my-app/src/app/[lng]/list/page.tsx
+++ my-app/src/app/[lng]/list/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 import { FC, Fragment, useState, useEffect, useCallback } from "react";
 import { FaStar } from "react-icons/fa6";
-import {
-  fetchMonsterLocation
-} from "@/components/api/MLApi";
+import { fetchMonsterLocation } from "@/components/api/MLApi";
 import { useTranslation } from "react-i18next";
-
 
 const ListPage: FC = () => {
   const { t } = useTranslation("monster");
@@ -31,56 +28,53 @@ const ListPage: FC = () => {
       monsterNames: item.name.split(",").slice(0, 2),
     })) || [];
 
-    const translatedMonsterNamesString = function(monsterNames : string []) { 
-      const trans = monsterNames.map((monsterName) => {
-        return transMonster(`equipSetting.${monsterName}.name`);
-      })
-      return trans.join(", ");
-    }
+  const translatedMonsterNamesString = function (monsterNames: string[]) {
+    const trans = monsterNames.map((monsterName) => {
+      return transMonster(`equipSetting.${monsterName}.name`);
+    });
+    return trans.join(", ");
+  };
 
   return (
     <div className="container">
-      <table className="w-full text-left table-auto min-w-max" >
+      <table className="w-full text-left table-auto min-w-max mb-6">
         <thead>
-          <tr>
-          <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">時間</th>
-          <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">魔物星數</th>
-          <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">魔物名稱</th>
-          <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">週目</th>
-          <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">座標</th></tr>
+          <tr className="bg-slate-200 dark:bg-slate-800">
+            <th className="table-head">時間</th>
+            <th className="table-head">魔物星數</th>
+            <th className="table-head">魔物名稱</th>
+            <th className="table-head">週目</th>
+            <th className="table-head">座標</th>
+          </tr>
         </thead>
         <tbody>
-          {
-            processedData.map((item) => {
-              return (
-                <tr key={item.id} >
-                  <td className="p-4 border-b border-blue-gray-50">
-                    {(() => {
-                      const date = new Date(item.createdAt + "Z");
-                      const localTime = date.toLocaleString(undefined, {
-                        hour12: false,
-                      });
-                      return localTime;
-                    })()}
-                  </td >
-                  <td className="p-4 border-b border-blue-gray-50" >
-                    { item.level }
-                  </td>
-                  <td className="p-4 border-b border-blue-gray-50">
-                  {
-                    translatedMonsterNamesString(item.monsterNames)
-                  }  
-                  </td>
-                  <td className="p-4 border-b border-blue-gray-50">
-                    {item.round} {t("MonsterMap.round")}
-                  </td>
-                  <td className="p-4 border-b border-blue-gray-50">
-                    {item.coordinates}
-                  </td>
-                </tr>
-              )
-            })
-          }
+          {processedData.map((item) => {
+            return (
+              <tr key={item.id} className="table-body">
+                <td data-td="時間" className="table-list">
+                  {(() => {
+                    const date = new Date(item.createdAt + "Z");
+                    const localTime = date.toLocaleString(undefined, {
+                      hour12: false,
+                    });
+                    return localTime;
+                  })()}
+                </td>
+                <td data-td="魔物星數" className="table-list">
+                  {item.level}
+                </td>
+                <td data-td="魔物名稱" className="table-list">
+                  {translatedMonsterNamesString(item.monsterNames)}
+                </td>
+                <td data-td={`${t("MonsterMap.round")}`} className="table-list">
+                  {item.round} {t("MonsterMap.round")}
+                </td>
+                <td data-td="座標" className="table-list">
+                  {item.coordinates}
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
