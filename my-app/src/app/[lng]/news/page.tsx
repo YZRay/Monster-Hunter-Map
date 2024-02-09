e0dd95c0ff13bb2754c525c9091159b2bb08c726
YZRay <yao19991119@gmail.com>
2024-02-09 23:56:07 +0800

    新增query pages

--- my-app/src/app/[lng]/news/page.tsx
+++ my-app/src/app/[lng]/news/page.tsx
@@ -2,24 +2,41 @@
 import NewsCard from "@/components/UI/NewsCard";
 import { allPosts } from "contentlayer/generated";
 import { Pagination } from "@nextui-org/react";
-import { useState } from "react";
-
+import { useState, useCallback, useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
 const { compareDesc } = require("date-fns");
 
 export default function Home() {
   const posts = allPosts.sort((a, b) =>
     compareDesc(new Date(a.date), new Date(b.date))
   );
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const queryPage = searchParams.get("pages");
+  const pages = Number(queryPage);
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const postsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState(pages || 1);
+  const postsPerPage = 6;
 
   const startIndex = (currentPage - 1) * postsPerPage;
 
   const endIndex = startIndex + postsPerPage;
   const displayedPosts = posts.slice(startIndex, endIndex);
-
   const totalPages = Math.ceil(posts.length / postsPerPage);
+
+  const handlePageChange = useCallback(
+    (newPage: number) => {
+      setCurrentPage(newPage);
+      router.push(`/news?pages=${newPage}`);
+    },
+    [setCurrentPage, router]
+  );
+
+  useEffect(() => {
+    if (currentPage > totalPages) {
+      handlePageChange(1);
+    }
+  }, [currentPage, totalPages, handlePageChange]);
 
   return (
     <main>
@@ -41,7 +58,7 @@ export default function Home() {
               boundaries={2}
               size="lg"
               initialPage={currentPage}
-              onChange={(page) => setCurrentPage(page)}
+              onChange={handlePageChange}
             />
           </div>
           <aside className="col-span-3">
