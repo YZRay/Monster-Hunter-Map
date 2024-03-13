0ad56c33cce31f8ade443c5606bf93b7af67b6cc
YZRay <yao19991119@gmail.com>
2024-03-13 20:35:37 +0800

    拿掉react query devtools

--- my-app/src/app/[lng]/home/page.tsx
+++ my-app/src/app/[lng]/home/page.tsx
@@ -2,13 +2,11 @@
 import React, { FC } from "react";
 import QueryProvider from "@/components/Providers/QueryProvider";
 import MapSelection from "@/components/MapSelection";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const HomePage: FC = () => {
   return (
     <QueryProvider>
       <div className="container">
-        <ReactQueryDevtools />
         <MapSelection />
       </div>
     </QueryProvider>
